7bc619616c7d4c0ba1c089d34698e70a61156760
Sushant Paudel <paudelsushant482@gmail.com>
2020-01-03 09:39:02 +0545

    Adding of feature to watermark text over image

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ A powerful watermark library based on Jimp for node.js. This can be used to over
 var watermark = require('jimp-watermark');
 
 watermark.addWatermark('/path/to/image/file','/path/to/image/watermark', options);
+
+watermark.addTextWatermark('/path/to/image/file', options);
 ```
 
 ### API
@@ -55,11 +57,56 @@ var options = {
     'opacity': 0.6, //Should be less than one
     'dstPath' : './watermark.jpg'
 };
-watermark.addWatermark('./img/main.jpg', './img/logo.png',options);
+watermark.addWatermark('./img/main.jpg', './img/logo.png', options);
 
 ```
+
+#### addTextWatermark(imageSource, options)
+
+API to overlay watermark in given image. It takes two arguments : 
+1. path of the image
+2. options object. This argument is optional
+
+
+**Options**
+
+Various options supported by this API are :
+- **text** - To specify the text to be overlaid on the main image.
+- **textSize** - To specify size of text over the main image, value ranged from 1 to 8.
+- **dstPath** - To specify the output path. Default is 'watermark.{sourceFile ext}'.
+
+**Example**
+
+```javascript
+var watermark = require('jimp-watermark');
+
+watermark.addTextWatermark('./img/main.jpg').then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
+```
+
+**Different Options**
+
+```javascript
+
+//
+// Options to specify output path
+//
+var watermark = require('jimp-watermark');
+var options = {
+	'text': 'watermark-test',
+    'textSize': 6, //Should be between 1-8
+    'dstPath' : './watermark.jpg'
+};
+watermark.addTextWatermark('./img/main.jpg', options);
+```
+
 ### Inspiration
 [https://github.com/luthraG/image-watermark](https://github.com/luthraG/image-watermark)
+
+[Image Processing in NodeJS with Jimp - Medium](https://medium.com/@rossbulat/image-processing-in-nodejs-with-jimp-174f39336153)
 
 ### License(MIT)
 
--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const defaultOptions = {
     ratio: 0.6,
     opacity: 0.6,
     dstPath: './watermark.jpg',
+    text: 'jimp-watermark',
+    textSize: 1,
 }
 
+
+const SizeEnum = Object.freeze({
+    1: Jimp.FONT_SANS_8_BLACK,
+    2: Jimp.FONT_SANS_10_BLACK,
+    3: Jimp.FONT_SANS_12_BLACK,
+    4: Jimp.FONT_SANS_14_BLACK,
+    5: Jimp.FONT_SANS_16_BLACK,
+    6: Jimp.FONT_SANS_32_BLACK,
+    7: Jimp.FONT_SANS_64_BLACK,
+    8: Jimp.FONT_SANS_128_BLACK,
+})
+const ErrorTextSize = new Error("Text size must range from 1 - 8");
 const ErrorScaleRatio = new Error("Scale Ratio must be less than one!");
 const ErrorOpacity = new Error("Opacity must be less than one!");
 
@@ -32,6 +46,50 @@ const checkOptions = (options) => {
     return options;
 }
 
+/**
+ * @param {String} mainImage - Path of the image to be watermarked
+ * @param {Object} options
+ * @param {String} options.text     - String to be watermarked
+ * @param {Number} options.textSize - Text size ranging from 1 to 8
+ * @param {String} options.dstPath  - Destination path where image is to be exported
+ */
+module.exports.addTextWatermark = async (mainImage, options) => {
+    try {
+        options = checkOptions(options);
+        const main = await Jimp.read(mainImage);
+        const maxHeight = main.getHeight();
+        const maxWidth = main.getWidth();
+        if (Object.keys(SizeEnum).includes(String(options.textSize))) {
+            const font = await Jimp.loadFont(SizeEnum[options.textSize]);
+            const X = 0,        //Always center aligned
+                Y = 0
+            const finalImage = await main.print(font, X, Y, {
+                text: options.text,
+                alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+                alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+            }, maxWidth, maxHeight);
+            finalImage.quality(100).write(options.dstPath);
+            return {
+                destinationPath: options.dstPath,
+                imageHeight: main.getHeight(),
+                imageWidth: main.getWidth(),
+            };
+        } else {
+            throw ErrorTextSize;
+        }
+    } catch (err) {
+        throw err;
+    }
+}
+
+/**
+ * @param {String} mainImage - Path of the image to be watermarked
+ * @param {String} watermarkImage - Path of the watermark image to be applied
+ * @param {Object} options
+ * @param {Float} options.ratio     - Ratio in which the watermark is overlaid
+ * @param {Float} options.opacity   - Value of opacity of the watermark image during overlay
+ * @param {String} options.dstPath  - Destination path where image is to be exported
+ */
 module.exports.addWatermark = async (mainImage, watermarkImage, options) => {
     try {
         options = checkOptions(options);
@@ -39,8 +97,8 @@ module.exports.addWatermark = async (mainImage, watermarkImage, options) => {
         const watermark = await Jimp.read(watermarkImage);
         const [newHeight, newWidth] = getDimensions(main.getHeight(), main.getWidth(), watermark.getHeight(), watermark.getWidth(), options.ratio);
         watermark.resize(newWidth, newHeight);
-        const positionX = (main.getWidth() - newWidth) / 2;
-        const positionY = (main.getHeight() - newHeight) / 2;
+        const positionX = (main.getWidth() - newWidth) / 2;     //Centre aligned
+        const positionY = (main.getHeight() - newHeight) / 2;   //Centre aligned
         watermark.opacity(options.opacity);
         main.composite(watermark,
             positionX,
